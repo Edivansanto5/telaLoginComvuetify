94374a0ceaaae218f70cb5d93851f203901a3547
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-12 12:33:40 -0300

    criando os inputes de coletar os nomes do usuario

--- formulario-vuetify/src/components/RegistrationForm.vue
+++ formulario-vuetify/src/components/RegistrationForm.vue
@@ -2,7 +2,8 @@
   <v-container fluid class="fluid">
     <v-row justify="center" align="center" class="row">
 
-      <v-col cols="6" sm="12" md="6" xs="12" class="text-center purple darken-2" style="height: 100vh">
+      <v-col cols="6" sm="12" md="6" xs="12" class="text-center purple darken-2"
+        style="height: 100vh">
 
         <v-row justify="center" align="center" style="height: 80vh">
           <v-col>
@@ -13,14 +14,27 @@
       </v-col>
 
       <v-col cols="6" sm="12" md="6" xs="12" class="text-center" style="height: 100vh">
-        <h1>Bem-Vindo ao LogRockets</h1>
-        <p>Preencha este formulário para criar uma conta</p>
+        <h1>Welcome to LogRockets</h1>
+        <p>Please complete this form to create an account</p>
+
+         <v-form ref="form" class="mx-2" lazy-validation> 
+            <v-row>
+               <v-col cols="6">
+                  <v-text-field v-model="firstname" :rules="nameRules" label="Primeiro Nome">
+                  </v-text-field>
+               </v-col>
+
+               <v-col cols="6">
+                  <v-text-field v-model="lastname" :rules="nameRules" label="Segundo Nome"/>
+               </v-col>
+
+            </v-row>
+         </v-form>
       </v-col>
 
     </v-row>
   </v-container>
 </template>
-
 <script>
 
 export default {
