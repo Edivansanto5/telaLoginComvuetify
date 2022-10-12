4d842eec234ade7d6985b65ad9bb41d2f1a7b6bf
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-12 12:16:27 -0300

    finalizando a estrutura do formulario

--- formulario-vuetify/src/components/RegistrationForm.vue
+++ formulario-vuetify/src/components/RegistrationForm.vue
@@ -1,12 +1,24 @@
 <template>
+  <v-container fluid class="fluid">
+    <v-row justify="center" align="center" class="row">
 
-   <v-conteiner fluid class="class">
-      <h1>teste01</h1>
-    <v-row>
-      
+      <v-col cols="6" sm="12" md="6" xs="12" class="text-center purple darken-2" style="height: 100vh">
+
+        <v-row justify="center" align="center" style="height: 80vh">
+          <v-col>
+            <h1 class="white--text">LogRockets</h1>
+          </v-col>
+        </v-row>
+
+      </v-col>
+
+      <v-col cols="6" sm="12" md="6" xs="12" class="text-center" style="height: 100vh">
+        <h1>Welcome to LogRockets</h1>
+        <p>Please complete this form to create an account</p>
+      </v-col>
+
     </v-row>
-
-   </v-conteiner>
+  </v-container>
 </template>
 
 <script>
