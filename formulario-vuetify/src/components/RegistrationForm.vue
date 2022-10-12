ca6998661479849b7f18b304641659557c6c90c7
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-12 11:43:06 -0300

    teste

--- formulario-vuetify/src/components/RegistrationForm.vue
+++ formulario-vuetify/src/components/RegistrationForm.vue
@@ -1,9 +1,16 @@
 <template>
-    
+
+   <v-conteiner fluid class="class">
+    <v-row>
+        <h1>teste</h1>
+    </v-row>
+
+   </v-conteiner>
 </template>
+
 <script>
 
 export default {
-    
+   name:'RegistrationForm'
 }
 </script>
--- formulario-vuetify/src/views/HomeView.vue
+++ formulario-vuetify/src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
-  <hello-world />
+<div>
+  <registrationForm/>
+ 
+</div>
+
 </template>
 
 <script>
   import HelloWorld from '../components/HelloWorld'
+  import RegistrationForm from '../components/RegistrationForm.vue'
 
   export default {
     name: 'Home',
 
     components: {
       HelloWorld,
+        RegistrationForm,
+    
     },
   }
 </script>
